7f71399fcc171f69e9b454c94a546b383c28ec28
Benjamin Franzke <ben@bnf.dev>
2025-03-10 03:16:42 +0100

    Fix minification of empty export declaration from spidermonkey ast

--- test/mocha/spidermonkey.js
+++ test/mocha/spidermonkey.js
@@ -172,6 +172,22 @@ describe("spidermonkey export/import sanity test", function() {
         );
     });
 
+    it("should correctly minify spidermonkey AST with empty export declaration", async () => {
+        const code = "export {}";
+        const ast = acornParse(code, { sourceType: 'module', locations: true, ecmaVersion: 2015 });
+        const result = await minify(ast, { ecma: 2015, module: true, parse: { spidermonkey: true } });
+        assert.strictEqual(
+            result.code,
+            "export{};"
+        );
+
+        const vanilla = await minify(code, { ecma: 2015, module: true });
+        assert.strictEqual(
+            result.code,
+            vanilla.code
+        );
+    });
+
     it("should correctly minify AST from from_moz_ast with default function parameter", async () => {
         const code = "function run(x = 2){}";
         const acornAst = acornParse(code, { locations: true, ecmaVersion: 2023 });
